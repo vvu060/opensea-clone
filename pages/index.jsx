e9a2950785183fa252e1b64b7bb9681e79a9ddc7
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-30 11:38:48 +0530

    add user login functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,41 @@
-import Head from 'next/head'
+import { useWeb3 } from '@3rdweb/hooks'
 
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
-export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42]`,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text-[#282b2f] font-semibold mt-4`,
+}
 
-      <Header />
-      <Hero />
+export default function Home() {
+  const { address, connectWallet } = useWeb3()
+
+  console.log(address)
+
+  return (
+    <div className={style.wrapper}>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={() => connectWallet('injected')}
+          >
+            Connect Wallet
+          </button>
+          <div>
+            You need chrome to
+            <br /> run this app.
+          </div>
+        </div>
+      )}
     </div>
   )
 }
