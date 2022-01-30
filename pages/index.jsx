5f612d09a5ca19bf36465d671552f383c7d3b81a
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-30 14:21:50 +0530

    implement toast notification on login

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import { useWeb3 } from '@3rdweb/hooks'
 import { useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 
 import Header from '../components/Header'
 import Hero from '../components/Hero'
@@ -15,6 +16,18 @@ const style = {
 export default function Home() {
   const { address, connectWallet } = useWeb3()
 
+  const welcomeUser = (userName, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back ${userName !== 'Unnamed' ? userName : ''}`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        },
+      }
+    )
+  }
+
   useEffect(() => {
     if (!address) return
     ;(async () => {
@@ -25,11 +38,14 @@ export default function Home() {
         walletAddress: address,
       }
       const result = await client.createIfNotExists(userDoc)
+
+      welcomeUser(result.userName)
     })()
   }, [address])
 
   return (
     <div className={style.wrapper}>
+      <Toaster position="top-center" reverseOrder={false} />
       {address ? (
         <>
           <Header />
